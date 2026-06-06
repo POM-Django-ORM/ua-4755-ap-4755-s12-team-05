05fabc900a806af7a63e6c2e59626b606499a22e
Vira Nychkalo <nychkalovira@gmail.com>
2026-06-06 23:01:08 +0300

    updetad book and author

--- library/author/models.py
+++ library/author/models.py
@@ -3,29 +3,33 @@ from django.db import models
 
 class Author(models.Model):
     """
-        This class represents an Author. \n
-        Attributes:
-        -----------
-        param name: Describes name of the author
-        type name: str max_length=20
-        param surname: Describes last name of the author
-        type surname: str max_length=20
-        param patronymic: Describes middle name of the author
-        type patronymic: str max_length=20
-
+    This class represents an Author. 
+    Attributes:
+    -----------
+    param name: Describes name of the author
+    type name: str max_length=20
+    param surname: Describes last name of the author
+    type surname: str max_length=20
+    param patronymic: Describes middle name of the author
+    type patronymic: str max_length=20
     """
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    patronymic = models.CharField(max_length=20)
 
     def __str__(self):
         """
         Magic method is redefined to show all information about Author.
         :return: author id, author name, author surname, author patronymic
         """
+        return f"{self.id}: {self.name} {self.surname} {self.patronymic}"
 
     def __repr__(self):
         """
         This magic method is redefined to show class and id of Author object.
         :return: class, id
         """
+        return f"{self.__class__.__name__}(id={self.id})"
 
     @staticmethod
     def get_by_id(author_id):
@@ -33,6 +37,10 @@ class Author(models.Model):
         :param author_id: SERIAL: the id of a Author to be found in the DB
         :return: author object or None if a user with such ID does not exist
         """
+        try:
+            return Author.objects.get(id=author_id)
+        except Author.DoesNotExist:
+            return None
 
     @staticmethod
     def delete_by_id(author_id):
@@ -41,6 +49,12 @@ class Author(models.Model):
         :type author_id: int
         :return: True if object existed in the db and was removed or False if it didn't exist
         """
+        try:
+            author = Author.objects.get(id=author_id)
+            author.delete()
+            return True
+        except Author.DoesNotExist:
+            return False
 
     @staticmethod
     def create(name, surname, patronymic):
@@ -53,6 +67,14 @@ class Author(models.Model):
         type patronymic: str max_length=20
         :return: a new author object which is also written into the DB
         """
+        if name and len(name) <= 20 and surname and len(surname) <= 20 and patronymic and len(patronymic) <= 20:
+            author = Author.objects.create(
+                name=name, 
+                surname=surname, 
+                patronymic=patronymic
+            )
+            return author
+        return None
 
     def to_dict(self):
         """
@@ -65,6 +87,12 @@ class Author(models.Model):
         |   'patronymic': 'ln',
         | }
         """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'surname': self.surname,
+            'patronymic': self.patronymic
+        }
 
     def update(self,
                name=None,
@@ -80,9 +108,17 @@ class Author(models.Model):
         type patronymic: str max_length=20
         :return: None
         """
+        if name is not None:
+            self.name = name
+        if surname is not None:
+            self.surname = surname
+        if patronymic is not None:
+            self.patronymic = patronymic
+        self.save()
 
     @staticmethod
     def get_all():
         """
         returns data for json request with QuerySet of all authors
         """
+        return list(Author.objects.all())
--- library/book/models.py
+++ library/book/models.py
@@ -1,32 +1,38 @@
 from django.db import models
 
-
 class Book(models.Model):
     """
-        This class represents an Book. \n
-        Attributes:
-        -----------
-        param name: Describes name of the book
-        type name: str max_length=128
-        param description: Describes description of the book
-        type description: str
-        param count: Describes count of the book
-        type count: int default=10
-        param authors: list of Authors
-        type authors: list->Author
+    This class represents a Book. 
+    Attributes:
+    -----------
+    param name: Describes name of the book
+    type name: str max_length=128
+    param description: Describes description of the book
+    type description: str
+    param count: Describes count of the book
+    type count: int default=10
+    param authors: list of Authors
+    type authors: list->Author
     """
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    count = models.IntegerField(default=10)
+    authors = models.ManyToManyField('author.Author', related_name='books')
 
     def __str__(self):
         """
         Magic method is redefined to show all information about Book.
         :return: book id, book name, book description, book count, book authors
         """
+        author_ids = [author.id for author in self.authors.all()]
+        return f"ID: {self.id}, Name: {self.name}, Description: {self.description}, Count: {self.count}, Authors: {author_ids}"
 
     def __repr__(self):
         """
         This magic method is redefined to show class and id of Book object.
         :return: class, id
         """
+        return f"{self.__class__.__name__}(id={self.id})"
 
     @staticmethod
     def get_by_id(book_id):
@@ -34,6 +40,10 @@ class Book(models.Model):
         :param book_id: SERIAL: the id of a Book to be found in the DB
         :return: book object or None if a book with such ID does not exist
         """
+        try:
+            return Book.objects.get(id=book_id)
+        except Book.DoesNotExist:
+            return None
 
     @staticmethod
     def delete_by_id(book_id):
@@ -42,6 +52,12 @@ class Book(models.Model):
         :type book_id: int
         :return: True if object existed in the db and was removed or False if it didn't exist
         """
+        try:
+            book = Book.objects.get(id=book_id)
+            book.delete()
+            return True
+        except Book.DoesNotExist:
+            return False
 
     @staticmethod
     def create(name, description, count=10, authors=None):
@@ -56,6 +72,17 @@ class Book(models.Model):
         type authors: list->Author
         :return: a new book object which is also written into the DB
         """
+        if name and len(name) <= 128:
+            book = Book.objects.create(
+                name=name, 
+                description=description, 
+                count=count
+            )
+            if authors:
+                # Використовуємо .set() для додавання списку авторів після створення книги
+                book.authors.set(authors)
+            return book
+        return None
 
     def to_dict(self):
         """
@@ -65,14 +92,21 @@ class Book(models.Model):
         |   'id': 8,
         |   'name': 'django book',
         |   'description': 'bla bla bla',
-        |   'count': 10',
+        |   'count': 10,
         |   'authors': []
         | }
         """
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'count': self.count,
+            'authors': [author.id for author in self.authors.all()]
+        }
 
     def update(self, name=None, description=None, count=None):
         """
-        Updates book in the database with the specified parameters.\n
+        Updates book in the database with the specified parameters.
         param name: Describes name of the book
         type name: str max_length=128
         param description: Describes description of the book
@@ -81,23 +115,36 @@ class Book(models.Model):
         type count: int default=10
         :return: None
         """
+        if name is not None:
+            self.name = name
+        if description is not None:
+            self.description = description
+        if count is not None:
+            self.count = count
+        self.save()
 
     def add_authors(self, authors):
         """
-        Add  authors to  book in the database with the specified parameters.\n
+        Add authors to book in the database with the specified parameters.
         param authors: list authors
         :return: None
         """
+        if authors:
+            # Символ * розпаковує список, оскільки метод .add() приймає аргументи через кому
+            self.authors.add(*authors)
 
     def remove_authors(self, authors):
         """
-        Remove authors to  book in the database with the specified parameters.\n
+        Remove authors to book in the database with the specified parameters.
         param authors: list authors
         :return: None
         """
+        if authors:
+            self.authors.remove(*authors)
 
     @staticmethod
     def get_all():
         """
         returns data for json request with QuerySet of all books
         """
+        return list(Book.objects.all())
